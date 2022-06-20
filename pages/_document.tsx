dffc927e87c68cd764c05d61538527d5bfb85818
ghalib77 <muhamadghalib7@gmail.com>
2022-06-20 15:56:30 +0700

    feat: add custom document

---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => App,
+        enhanceComponent: (Component) => Component,
+      });
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;800;900&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Main />
+        <NextScript />
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
