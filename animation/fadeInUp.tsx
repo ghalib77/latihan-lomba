d26ef0cc06bb18f08d37166257d2745fd16909c4
ghalib77 <muhamadghalib7@gmail.com>
2022-06-20 17:41:17 +0700

    feat: add fade in up animation

---
+++ animation/fadeInUp.tsx
@@ -0,0 +1,30 @@
+interface FadeInUp {
+  yHide?: number;
+  yVisible?: number;
+  delay?: number;
+}
+const ease = [0.075, 0.82, 0.165, 1];
+
+const fadeInUp = ({ yHide, yVisible, delay }: FadeInUp) => ({
+  initial: {
+    y: yHide,
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  animate: {
+    y: yVisible,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: delay,
+      ease: ease,
+      stiffness: 150,
+      type: "spring",
+      damping: 4,
+    },
+  },
+});
+
+export { fadeInUp };
